26e9460c2910e0172bb3fe1dbbc023d258263c80
aybatova <evaybatova@gmail.com>
2019-01-28 12:47:04 +0300

    initial commit

---
+++ src/main/resources/sql/data-h2.sql
@@ -0,0 +1,4 @@
+INSERT INTO wishlist (id, name, content)VALUES
+(1, 'wish1', 'Кожаный кошелек MERGEN'),
+(2, 'wish2', 'Чтоб не выпадали из ушей с шумоподавлением'),
+(3, 'wish3', 'Духи ECLAT LANVINобъем 50 мл');
